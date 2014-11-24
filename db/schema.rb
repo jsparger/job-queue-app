ef7f845d89cb9119c6753d27ee917384a572b1a9
John Sparger <jsparger87@gmail.com>
2014-11-24 15:35:03 -0500

    added role to users table. Set up some role based abilities.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119212525) do
+ActiveRecord::Schema.define(version: 20141124202546) do
 
   create_table "job_queues", force: true do |t|
     t.integer  "Job_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141119212525) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
